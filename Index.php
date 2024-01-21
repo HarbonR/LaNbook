a7b402b6d1987d7955a4c0b3993b024a87c4c909
Pavel <Stydent86nv@mail.ru>
2024-01-21 18:53:44 +0500

    first version

---
+++ Index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaNbook</title>
+    <link rel="stylesheet" href="Style/style.css">
+</head>
+<body>
+    <div class="menu">
+        <div class="menu__logo">
+            <div class="menu__logo-icon">
+                LB
+            </div>
+            <div class="menu__logo-text">
+                LaNbook
+            </div>
+        </div>
+        <div class="menu__grammar">
+            <div class="menu__icon">
+                <img src="picture/icon/grammar-icon.svg" alt="">
+            </div>
+            <div class="menu__text">
+                Грамматика
+            </div>
+        </div>
+        <div class="menu__cards menu_active">
+            <div class="menu__icon">
+                <svg width="35" height="45" viewBox="0 0 35 45" class = "svg_active">
+                    <path d="M19.7754 34.4531H3.95508C1.77416 34.4531 0 32.679 0 30.498V3.95508C0 1.77416 1.77416 0 3.95508 0H19.7754C21.9563 0 23.7305 1.77416 23.7305 3.95508V30.498C23.7305 32.679 21.9563 34.4531 19.7754 34.4531Z"/>
+                    <path d="M30.3223 10.7227H26.3672V30.498C26.3672 34.1326 23.4099 37.0898 19.7754 37.0898H10.5469V41.0449C10.5469 43.2258 12.321 45 14.502 45H30.3223C32.5032 45 34.2773 43.2258 34.2773 41.0449V14.6777C34.2773 12.4968 32.5032 10.7227 30.3223 10.7227Z"/>
+                </svg>
+            </div>
+            <div class="menu__text menu__text_active">
+                Карточки
+            </div>
+        </div>
+        <div class="menu__training">
+            <div class="menu__icon">
+                <img src="picture/icon/training-icon.svg" alt="">
+            </div>
+            <div class="menu__text">
+                Тренировки
+            </div>
+        </div>
+        <div class="menu__personal-area">
+            <div class="menu__icon">
+                <img src="picture/icon/personal-area-icon.svg" alt="">
+            </div>
+            <div class="menu__text">
+                Личный кабинет
+            </div>
+        </div>
+    </div>
+    <script src=""></script>
+</body>
+</html>
